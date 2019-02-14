8fda30c92ebfede824fb673a09c25157d374a29f
unknown <paweennuch.boonamnuay@gmail.com>
2019-02-14 16:29:05 +0200

    Updates

---
+++ precache-manifest.88f4cffb3875b9f12e827a090c225e55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6beab6f7b11d991665b7",
+    "url": "/Blog-Application/static/js/runtime~main.6beab6f7.js"
+  },
+  {
+    "revision": "42a5356e7494b601a504",
+    "url": "/Blog-Application/static/js/main.42a5356e.chunk.js"
+  },
+  {
+    "revision": "592705258be2deabba67",
+    "url": "/Blog-Application/static/js/1.59270525.chunk.js"
+  },
+  {
+    "revision": "42a5356e7494b601a504",
+    "url": "/Blog-Application/static/css/main.7a0bd1fa.chunk.css"
+  },
+  {
+    "revision": "b6c990cf4a8fb8cf3f52d5a871299375",
+    "url": "/Blog-Application/index.html"
+  }
+];
